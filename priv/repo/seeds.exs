60ad0a9583eda6138b3e4b2a9777f519fc3d3be3
Carlos Rocha <carlosrocha@mecajun.com>
2024-06-14 09:51:53 -0300

    Adding seed script.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,42 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Foodtrunk.Repo.insert!(%Foodtrunk.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule InitialSeed do
+  defp parse(file_path) do
+    File.stream!(file_path)
+    |> Stream.drop(1) # skipping first row
+    |> Stream.map(&String.split(&1, ","))
+  end
+  # locationid,Applicant,FacilityType,cnn,LocationDescription,Address,blocklot,block,lot,permit,Status,FoodItems,X,Y,Latitude,Longitude,Schedule,dayshours,NOISent,Approved,Received,PriorPermit,ExpirationDate,Location,Fire Prevention Districts,Police Districts,Supervisor Districts,Zip Codes,Neighborhoods (old)
+  defp insert_food_truck(values) do
+    [_,applicant,facilityType,_,locationDescription,address,_,_,_,_,_,foodItems,_,_,latitude,longitude,schedule,dayshours | _] = values
+
+    Hello.Repo.transaction(fn ->
+      Hello.FoodTrucks.Create.call(%{
+        name: applicant,
+        type: facilityType,
+        location: locationDescription,
+        address: address,
+        food_items: foodItems,
+        latitude: latitude,
+        longitude: longitude,
+        schedule_url: schedule,
+        schedule: dayshours
+      })
+      |> case do
+        {:ok, _} -> IO.puts("Record created for #{applicant}")
+        {:error, changeset} -> IO.puts("Error creating record: #{inspect(changeset.errors)}")
+      end
+    end)
+
+  end
+
+  defp import_from_csv(csv_file) do
+    parse(csv_file)
+    |> Enum.each(&insert_food_truck/1)
+  end
+
+  def run() do
+    file_name = Path.join([:code.priv_dir(:hello), "Mobile_Food_Facility_Permit.csv"])
+    import_from_csv(file_name)
+  end
+end
+
+InitialSeed.run()
